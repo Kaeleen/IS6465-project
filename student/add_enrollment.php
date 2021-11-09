67e3a3c6a4feac20ea9b426587c180214e2d1e2d
Kaeleen <kaeleen1211@gmail.com>
2021-11-09 14:14:27 -0700

    student_index page fixed

--- student/add_enrollment.php
+++ student/add_enrollment.php
@@ -1,14 +1,23 @@
 <?php
 include '../mysql.php';
-$sql = "select * from course where year = '2021' and semester = 'Fall'";
-$result = mysqli_query($conn,$sql);
 session_start();
-if(isset($_SESSION['student'])){
-}else{
-    header('Refresh:0.0001;url=login.php');
-    echo "<script> alert('illegal access!')</script>";
-    exit();
+$student_id = $_SESSION['student_id'];
+$sql = "select student.*,program.program_name from student join program on program.program_id = student.program_id and student_id=$student_id = $student_id;
+";
+$result = mysqli_query($conn,$sql);
+$student_info = mysqli_fetch_assoc($result);
+$name = $student_info['name'];
+
+$year_sql = "select * from course";
+$year_result = mysqli_query($conn,$year_sql);
+$year = $semester = [];
+while ($row = mysqli_fetch_assoc($year_result)) {
+    $year[] =$row['year'];
+    $semester[]=$row['semester'];
 }
+$year = array_unique($year);
+$semester = array_unique($semester);
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -16,7 +25,7 @@ if(isset($_SESSION['student'])){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Add enrollment Info</title>
+    <title>Update student info</title>
     <style>
         .adds-stu-wrap{
             width: 700px;
@@ -49,50 +58,130 @@ if(isset($_SESSION['student'])){
             width: 260px;
         }
     </style>
+    <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.js"></script>
 </head>
 <body>
-<div class="adds-stu-wrap">
-    <h2 class="head" style="text-align:center">Add enrollment Info</h2>
+<div class="adds-stu-wrap" style="display: block">
+    <h2 class="head" style="text-align:center"> Student Profile</h2>
     <div class="adds-stu">
-        <form action="add_enrollment_do.php" method="post">
-<!--            <div>-->
-<!--                <p>course_id:</p>-->
-<!--                <input type="text" name="course_id" id="">-->
-<!--            </div>-->
-            <div>
-                <p>course:</p>
-                <input type="hidden" name="student_id" value="<?php echo  $_SESSION['student_id'];  ?>" id="">
-                <select name="course_id">
-                    <?php
-                    if(mysqli_num_rows($result) > 0){
-                        while ($row = mysqli_fetch_assoc($result)) {
-                            ?>
-                            <option value ="<?php echo  $row['course_id'];  ?>"><?php echo  $row['name'];  ?></option>
-                            <?php
-                        }
-                    }else{
-                        echo 'No data';
-                    }
-                    ?>
+        <form action="edit_do.php" method="post">
+                    <div>
+                        <p>Name:</p>
+                        <input type="text" name="name" disabled id="" value="<?php echo  $student_info['name'];  ?>">
+                        <input type="hidden" name="id" id="" value="<?php echo  $student_info['student_id'];  ?>">
+                        <input type="hidden" name="method" id="" value="edit_student_do">
+                    </div>
+                    <div>
+                        <p>Tel:</p>
+                        <input type="text" name="tel" id=""  disabled value="<?php echo  $student_info['tel'];  ?>">
+                    </div>
+                    <div>
+                        <p>Address:</p>
+                        <input type="text" name="address" id="" disabled value="<?php echo  $student_info['address'];  ?>">
+                    </div>
+                     <div>
+                        <p>Email:</p>
+                        <input type="text" name="email" id="" disabled value="<?php echo  $student_info['email'];  ?>">
+                    </div>
+                    <div>
+                        <p>program:</p>
+                        <input type="text" name="program" id="" disabled value="<?php echo  $student_info['program_name'];  ?>">
+                    </div>
+                    <div>
+                        <p>Date of Birth:</p>
+                        <input type="text" name="dob" id=""  disabled value="<?php echo  $student_info['dob'];  ?>">
+                    </div>
+                    <div>
+                    </div>
+        </form>
+    </div>
+</div>
+
+<!--add begin-->
+<div class="adds-stu-wrap" style="display: block">
+    <h2 class="head" style="text-align:center">Add Course </h2>
+    <div class="adds-stu">
+        <form action="edit_do.php" method="post">
+            <div style="text-align: center;margin-top: 80px">
+                <p>Year:</p>
+                <input type="text" name="year" id="year" disabled value="2021">
+
+            </div>
+            <div style="text-align: center;margin-top: 80px">
+                <p>Semester:</p>
+                <input type="text" name="semester" id="semester" disabled value="Fall">
+
+            </div>
+            <div style="text-align: center;margin-top: 80px">
+                course:
+                <select name="course" id="course" onchange="get_session()">
+
+                </select>
+                session:
+                <select name="session" id="session">
+
                 </select>
             </div>
-<!--            <div>-->
-<!--                <p>grade:</p>-->
-<!--                <input type="text" name="grade" id="">-->
-<!--            </div>-->
-<!--            <div>-->
-<!--                <p>date_enrolled:</p>-->
-<!--                <input type="text" name="date_enrolled" id="">-->
-<!--            </div>-->
-<!--            <div>-->
-<!--                <p>date_dropped:</p>-->
-<!--                <input type="text" name="date_dropped" id="">-->
-<!--            </div>-->
-            <div style="text-align: center">
-                <button >Submit</button>
+            <div>
+                <input type="hidden" name="student" id="" value="<?php echo  $student_info['student_id'];  ?>">
+                <input type="hidden" name="method" id="" value="add_enrollment">
+            </div>
+            <div>
+                <button>add course</button>
             </div>
         </form>
     </div>
 </div>
+<!--add end-->
+
+
 </body>
+
+<script>
+    function get_course(){
+        var $year = $("#year").val(),
+            $semester  = $("#semester").val();
+        $.ajax({
+            type: "POST",
+            url: "edit_do.php",
+            data: {method:"get_course",year:$year,semester:$semester},
+            dataType: "json",
+            success: function(data){
+            console.log(data)
+                
+                var pIndex = -1;
+                var course = document.getElementById("course");
+                document.getElementById("course").innerHTML = "";
+                for (var i = 0; i < data.length; i++) {
+                    var op = new Option(data[i]['course_anme'],data[i]['course_id']);
+                    course.options.add(op);
+                }
+                get_session()
+            }
+        });
+    }
+    get_course()
+
+    function get_session(){
+        var courseid = $("#course").val();
+            $.ajax({
+            type: "POST",
+            url: "edit_do.php",
+            data: {method:"get_session",courseid:courseid},
+            dataType: "json",
+            success: function(data){
+                console.log(data)
+                var pIndex = -1;
+                var session = document.getElementById("session");
+                document.getElementById("session").innerHTML = "";
+                for (var i = 0; i < data.length; i++) {
+                    var op = new Option(data[i]['session_name'],data[i]['session_id']);
+                    session.options.add(op);
+                }
+            }
+        });
+    }
+
+</script>
 </html>
--- student/index_student.php
+++ student/index_student.php
@@ -1,11 +1,20 @@
 <?php
 include '../mysql.php';
 session_start();
-$name = $_SESSION['student'];
 $student_id = $_SESSION['student_id'];
+$sql = "select student.*,program.program_name from student join program on program.program_id = student.program_id and student_id=$student_id = $student_id;
+";
+$result = mysqli_query($conn,$sql);
+$student_info = mysqli_fetch_assoc($result);
+if (empty($student_info)){
+    echo ' Insertion fail!';
+    header("refresh:1;url=../login.php");
+    print('Loading...<br>Will redirect to login page after 1 seconds');
+}
+$name = $student_info['name'];
+
 $year_sql = "select * from course";
 $year_result = mysqli_query($conn,$year_sql);
-
 $year = $semester = [];
 while ($row = mysqli_fetch_assoc($year_result)) {
     $year[] =$row['year'];
@@ -23,71 +32,117 @@ while($row = mysqli_fetch_assoc($courseResult)) {
     $coursrIds[] = $row['course_id'];
 }
 $coursrIds = implode(',', $coursrIds);
-$sql = "select * from student where name = '$name' limit 1";
-$sql2 = "select enrollment.* ,course.* from enrollment join course on enrollment.course_id=course.course_id where enrollment.student_id = '$student_id' and  enrollment.course_id  in ($coursrIds)";
-
-$result = mysqli_query($conn,$sql);
+$sql2 = "select enrollment.* ,course.*,session .* from enrollment left join course on enrollment.course_id=course.course_id  left join session on enrollment.course_id=session.course_id  where enrollment.student_id = '$student_id' and  enrollment.course_id  in ($coursrIds) group by enrollment_id ";
 $result2 = mysqli_query($conn,$sql2);
-if(isset($_SESSION['student'])){
-}else{
-    header('Refresh:0.0001;url=login.php');
-    echo "<script> alert('illegal access!')</script>";
-    exit();
+$sql_GPA = "select sum( grade * credit)/sum(credit) as gpa from enrollment left join course on enrollment.course_id =course.course_id  where enrollment.student_id = '$student_id' and  enrollment.course_id  in ($coursrIds) group by enrollment_id ";
+$resultGPA = mysqli_query($conn,$sql_GPA);
+$resultGPA = mysqli_fetch_assoc($resultGPA);
+if ($resultGPA){
+    $resultGPA = $resultGPA['gpa'];
 }
- ?>
-
+$gpa = bcdiv($resultGPA ?? 0,1,2);
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Student Homepage</title>
+    <style>
+        .adds-stu-wrap{
+            width: 700px;
+            height: auto;
+            margin: 0 auto;
+            margin-top: 100px;
+
+        }
+        .adds-stu{
+            float: left;
+            width: 100%;
+            height: auto;
+            background-color: #eee;
+            padding: 15px 10px;
+        }
+        .adds-stu div{
+            float: left;
+            width: 100%;
+            margin-bottom: 20px;
+        }
+        .adds-stu div>p{
+            float: left;
+            width: 100px;
+            margin: 0 10px 0 0;
+            text-align: right;
+
+        }
+        .adds-stu div>input{
+            float: left;
+            width: 260px;
+        }
+    </style>
     <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <script src="https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.js"></script>
-    <title>Student_info</title>
-    <h3>logged user:<?php echo $_SESSION['student']?></h3>
-    <a style="margin: 10px" href="../login.php">log out</a>
-
 </head>
 <body>
+<div class="adds-stu-wrap" style="display: block">
+    <h2 class="head" style="text-align:center"> Student Profile</h2>
+    <div class="adds-stu">
+        <form action="edit_do.php" method="post">
+                    <div>
+                        <p>Name:</p>
+                        <input type="text" name="name" id="" value="<?php echo  $student_info['name'];  ?>">
+                        <input type="hidden" name="id" id="" value="<?php echo  $student_info['student_id'];  ?>">
+                        <input type="hidden" name="method" id="" value="edit_student_do">
+                    </div>
+                    <div>
+                        <p>Tel:</p>
+                        <input type="text" name="tel" id="" value="<?php echo  $student_info['tel'];  ?>">
+                    </div>
+                    <div>
+                        <p>Email:</p>
+                        <input type="text" name="email" id="" value="<?php echo  $student_info['email'];  ?>">
+                    </div>
 
-<nav style="text-align: center">
+                    <div>
+                        <p> Password:</p>
+                        <input type="text" name="password" id="" value="<?php echo  $student_info['password'];  ?>">
+                    </div>
+                    <div>
+                        <p>address:</p>
+                        <input type="text" name="address" id="" value="<?php echo  $student_info['address'];  ?>">
+                    </div>
+                    <div>
+                        <p>program:</p>
+                        <input type="text" name="program" id="" disabled value="<?php echo  $student_info['program_name'];  ?>">
+                    </div>
+                    <div>
+                        <p>Date of Birth:</p>
+                        <input type="text" name="dob" id="" value="<?php echo  $student_info['dob'];  ?>">
+                    </div>
+                    <div>
+                        <button>update</button>
+                    </div>
+        </form>
+    </div>
+</div>
 
-</nav>
-<h2 style="float:left;width:100%;margin-top:50px; text-align:center">INTI Course Registration System</h2>
-<table style="margin-top:60px" align="center" width="60%" border="" cellspacing="0" cellpadding="0">
 
-    <tr><th>id</th><th>Name</th><th>Program</th><th>Tel.</th><th>Options</th></tr>
-    <?php
-    while ($row = mysqli_fetch_assoc($result)) {
-    ?>
-            <tr style='background-color:aqua'>
+<div style="margin-top: 500px;text-align: center">
 
-                <td align="center"><?php echo  $row['student_id'];  ?></td>
-                <td align="center"><?php echo  $row['name'];  ?></td>
-                <td align="center"><?php echo  $row['program'];  ?></td>
-                <td align="center"><?php echo  $row['tel'];  ?></td>
-                <td align="center">
-                    <a href="edit.php?id=<?php echo  $row['student_id'];  ?>" style="color:forestgreen">Update</a>
-                </td>
-            </tr>
-    <?php
-        }
-    ?>
-</table>
+    <h2 class="head" style="text-align:center">Enrollment Info</h2>
 
-<div style="text-align:center;margin-top: 100px">
-    <a href="add_enrollment.php" style="padding:3px;font-size:16px;background-color:greenyellow">Add New Course</a>
+    <a href="add_enrollment.php?id=<?php echo  $student_info['student_id'];  ?>" style="padding:3px;font-size:16px;background-color:greenyellow">Add New Course</a>
 </div>
 <div style="text-align: center;margin-top: 80px">
     year:
     <select name="year" id="year">
         <?php
-          foreach ($year as $item){
-                ?>
-                <option value ="<?php echo  $item;  ?>" <?php if ($item == $year_get){ ?>selected="" <?php } ?> ><?php echo  $item;  ?></option>
-                <?php
-            }
+        foreach ($year as $item){
+            ?>
+            <option value ="<?php echo  $item;  ?>" <?php if ($item == $year_get){ ?>selected="" <?php } ?> ><?php echo  $item;  ?></option>
+            <?php
+        }
 
         ?>
     </select>
@@ -103,34 +158,86 @@ if(isset($_SESSION['student'])){
     </select>
     <button onclick="search()">go</button>
 </div>
-<table style="margin-top:60px" align="center" width="60%" border="" cellspacing="0" cellpadding="0">
-    <tr><th>course_id</th><th>year</th><th>semester</th><th>course</th><th>grade</th><th>date_enrolled</th><th>date_dropped</th></tr>
-    <?php
-    if ($result2){
-    if(mysqli_num_rows($result2) > 0){
-        while ($row = mysqli_fetch_assoc($result2)) {
+<table style="margin-top:100px" align="center"  width="60%" border="" cellspacing="0" cellpadding="0">
+    <tr><th>course</th><th>credit</th><th>grade</th><th>year</th><th>semester</th><th>session</th>
+        <?php
+        if($year_get == '2021' && $semester_get == 'Fall'){
             ?>
-            <tr style='background-color:aqua'>
-<!--                <td align="center">--><?php //echo  $row['enroll_id'];  ?><!--</td>-->
-                <td align="center"><?php echo  $row['course_id'];  ?></td>
-                <td align="center"><?php echo  $row['year'];  ?></td>
-                <td align="center"><?php echo  $row['semester'];  ?></td>
-                <td align="center"><?php echo  $row['name'];  ?></td>
-                <td align="center"><?php echo  $row['grade'];  ?></td>
-                <td align="center"><?php echo  $row['date_enrolled'];  ?></td>
-                <td align="center"><?php echo  $row['date_dropped'];  ?></td>
-            </tr>
+            <th>options</th>
+            <?php
+        }else{
+            ?>
+            <th>date_enrolled</th><th>date_dropped</th>
             <?php
         }
-    }}
+        ?>
+    </tr>
+    <?php
+    if ($result2){
+        if(mysqli_num_rows($result2) > 0){
+            while ($row = mysqli_fetch_assoc($result2)) {
+                ?>
+                <tr style='background-color:aqua'>
+                    <td align="center"><?php echo  $row['course_name'];  ?></td>
+                    <td align="center"><?php echo  $row['credit'];  ?></td>
+                    <td align="center"><?php echo  $row['grade'];  ?></td>
+                    <td align="center"><?php echo  $row['year'];  ?></td>
+                    <td align="center"><?php echo  $row['semester'];  ?></td>
+                    <td align="center"><?php echo  $row['session_name'];  ?></td>
+
+                    <?php
+                    if($year_get == '2021' && $semester_get == 'Fall'){
+                    ?>
+                    <td align="center" >
+                       <a href="#" onclick="delete_enrollment(<?php echo  $row['enrollment_id'];  ?>)" > delete</a>
+                    </td>
+                        <?php
+                    }else{
+                        ?>
+                        <td align="center"><?php echo  $row['date_enrolled'];  ?></td>
+                        <td align="center"><?php echo  $row['date_dropped'];  ?></td>
+                        <?php
+                    }
+                            ?>
+                </tr>
+                <?php
+            }
+        }}
     ?>
 </table>
+<div style="text-align: center">
+    <?php
+    if($year_get != '2021' || $semester_get != 'Fall'){
+    ?>
+    Team GPA:<?php echo  $gpa;  ?>
+    <?php
+    }
+    ?>
+</div>
+
 </body>
+
 <script>
-       function search(){
-           var $year = $("#year").val(),
-               $semester  = $("#semester").val();
-           location.href = 'index_student.php?year='+$year+'&semester='+$semester
-       }
+    function search(){
+        var $year = $("#year").val(),
+            $semester  = $("#semester").val();
+        location.href = 'index_student.php?year='+$year+'&semester='+$semester+'&id='+<?php echo  $student_id;  ?>
+    }
+
+
+
+    function delete_enrollment(enrollment_id){
+            $.ajax({
+                type: "POST",
+                url: "edit_student_do.php",
+                data: {method:"delete_enrollment",enrollment_id:enrollment_id},
+                dataType: "json",
+                success: function(data){
+                    var $year = $("#year").val(),
+                        $semester  = $("#semester").val();
+                    location.href = 'index_student.php?year='+$year+'&semester='+$semester+'&id='+<?php echo  $student_id;  ?>
+                }
+            });
+    }
 </script>
 </html>
