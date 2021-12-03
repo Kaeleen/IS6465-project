ee245aea464d922606afaf3cc14efff889e3c9d2
Kaeleen <kaeleen1211@gmail.com>
2021-12-03 14:53:21 -0700

    connection

--- student/mysql.php
+++ student/mysql.php
@@ -1,15 +1,12 @@
 <?php
 
-$servername = "localhost:3306";
-$username = "root";
-$password = "";
-$dbname = "project_new";
-$char   = 'utf8';
-// create connection
-$conn = mysqli_connect($servername, $username, $password,
-    $dbname);
-// check connection
-if (!$conn) {
-    die("Connectionfailed: " . mysqli_connect_error());
-}
-mysqli_set_charset($conn,$char);
+$hn = 'localhost:3306';
+$db = 'project_new';
+$un = 'root';
+$pw = ''; //for MAC 'root'
+
+
+$conn = new mysqli($hn, $un, $pw, $db);
+if($conn->connect_error) die($conn->connect_error);
+
+?>
